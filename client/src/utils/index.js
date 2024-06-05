cc197bdba16b03f8d6997e8271c2bb6c49e2426a
VEDANT ACHHARYA <achharyavedant@gmail.com>
2024-06-05 18:28:54 +0530

    first commit

---
+++ client/src/utils/index.js
@@ -0,0 +1,46 @@
+export const daysLeft = (deadline) => {
+<<<<<<< HEAD
+  const difference = new Date(deadline).getTime() - Date.now();
+  const remainingDays = difference / (1000 * 3600 * 24);
+
+  return remainingDays.toFixed(0);
+};
+
+export const calculateBarPercentage = (goal, raisedAmount) => {
+  const percentage = Math.round((raisedAmount * 100) / goal);
+
+  return percentage;
+};
+
+export const checkIfImage = (url, callback) => {
+  const img = new Image();
+  img.src = url;
+
+  if (img.complete) callback(true);
+
+  img.onload = () => callback(true);
+  img.onerror = () => callback(false);
+};
+=======
+    const difference = new Date(deadline).getTime() - Date.now();
+    const remainingDays = difference / (1000 * 3600 * 24);
+  
+    return remainingDays.toFixed(0);
+  };
+  
+  export const calculateBarPercentage = (goal, raisedAmount) => {
+    const percentage = Math.round((raisedAmount * 100) / goal);
+  
+    return percentage;
+  };
+  
+  export const checkIfImage = (url, callback) => {
+    const img = new Image();
+    img.src = url;
+  
+    if (img.complete) callback(true);
+  
+    img.onload = () => callback(true);
+    img.onerror = () => callback(false);
+  };
+>>>>>>> 768a8249c4f9c8b402105c224dd82c16241527df
